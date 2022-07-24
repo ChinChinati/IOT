b04b991b63445d61c4cb6ac20a6662bf895c4529
ChinChinati <106835636+ChinChinati@users.noreply.github.com>
2022-07-25 00:01:06 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,25 +1,4 @@
-/*
-   -- New project --
-   
-   This source code of graphical user interface 
-   has been generated automatically by RemoteXY editor.
-   To compile this code using RemoteXY library 3.1.8 or later version 
-   download by link http://remotexy.com/en/library/
-   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
-     - for ANDROID 4.11.1 or later version;
-     - for iOS 1.9.1 or later version;
-    
-   This source code is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.    
-*/
 
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// RemoteXY select connection mode and include library 
 #define REMOTEXY_MODE__ESP32CORE_WIFI_CLOUD
 #include <WiFi.h>
 
@@ -101,9 +80,3 @@ void loop()
         
         }
  
-  // TODO you loop code
-  // use the RemoteXY structure for data transfer
-  // do not call delay() 
-
-
-}
